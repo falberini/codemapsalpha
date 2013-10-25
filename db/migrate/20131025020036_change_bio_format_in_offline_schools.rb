e77f06b748b59878e13ed8b50c1751bf42cd0a1a
Francisco Alberini <f.alberini@gmail.com>
2013-10-24 19:32:08 -0700

    Deleting old migration files

--- db/migrate/20131025020036_change_bio_format_in_offline_schools.rb
+++
@@ -1,9 +0,0 @@
-class ChangeBioFormatInOfflineSchools < ActiveRecord::Migration
-  def up
-	  change_column :offline_schools, :bio, :string
-  end
-
-  def down
-  	change_column :offline_schools, :bio, :text
-  end
-end
