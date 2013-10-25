3edc79620e50ffb0839c2dc13a20d672f743a0a0
Francisco Alberini <f.alberini@gmail.com>
2013-10-24 18:43:26 -0700

    Fixed rake task error

--- app/models/offline_school.rb
+++ app/models/offline_school.rb
@@ -1,5 +1,5 @@
 class OfflineSchool < ActiveRecord::Base
   attr_accessible :bio, :city, :ed_level, :format, :name, :pricing, :site_url, :state, :zip_code, :street, :latitude, :longitude
   geocoded_by :street
-	after_validation :geocode, :if => :address_changed?
+	after_validation :geocode
 end
