3531090f3b0d822724912a571adfcf0fe343c306
Francisco Alberini <f.alberini@gmail.com>
2013-10-24 19:53:11 -0700

    New migration changing all columns to text

--- db/migrate/20131025022341_change_bio_format_in_offline_schools.rb
+++ db/migrate/20131025022341_change_bio_format_in_offline_schools.rb
@@ -1,5 +1,8 @@
 class ChangeBioFormatInOfflineSchools < ActiveRecord::Migration
  def change
 	  change_column :offline_schools, :bio, :text
+	  change_column :offline_schools, :site_url, :text
+	  change_column :offline_schools, :ed_level, :text
+	  change_column :offline_schools, :format, :text
   end
 end
