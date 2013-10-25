9d7e5656fdca0bf5a8106aed482eeaaa15710daf
Francisco Alberini <f.alberini@gmail.com>
2013-10-24 19:20:50 -0700

    Fixing bootstrap

--- db/migrate/20131025020036_change_bio_format_in_offline_schools.rb
+++ db/migrate/20131025020036_change_bio_format_in_offline_schools.rb
@@ -1,13 +1,9 @@
 class ChangeBioFormatInOfflineSchools < ActiveRecord::Migration
   def up
 	  change_column :offline_schools, :bio, :string
-	  change_column :offline_schools, :format, :string
-	  change_column :offline_schools, :ed_level, :string
   end
 
   def down
   	change_column :offline_schools, :bio, :text
-  	change_column :offline_schools, :format, :text
-  	change_column :offline_schools, :ed_level, :text
   end
 end
