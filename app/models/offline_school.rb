94e45316d5c1e61b3846ad29652dd99f09315596
Francisco Alberini <f.alberini@gmail.com>
2013-10-25 07:52:19 -0700

    Adding address validation

--- app/models/offline_school.rb
+++ app/models/offline_school.rb
@@ -1,5 +1,5 @@
 class OfflineSchool < ActiveRecord::Base
   attr_accessible :bio, :city, :ed_level, :format, :name, :pricing, :site_url, :state, :zip_code, :street, :latitude, :longitude
   geocoded_by :street
-	after_validation :geocode
+	after_validation :geocode, :if => :address_changed?
 end
