a81e6e8c574d1ba70000a41286f1a5520ed8725b
Francisco ALberini <f.alberini@gmail.com>
2014-01-07 16:36:50 -0800

    Adding new offline schools columns to attr accessible

--- app/models/offline_school.rb
+++ app/models/offline_school.rb
@@ -1,5 +1,5 @@
 class OfflineSchool < ActiveRecord::Base
-  attr_accessible :bio, :city, :ed_level, :format, :name, :pricing, :site_url, :state, :zip_code, :street, :latitude, :longitude
+  attr_accessible :bio, :city, :ed_level, :format, :name, :pricing, :site_url, :state, :zip_code, :street, :latitude, :longitude, :languages, :contact_email, :contact_number, :country
   validate do
 		errors.add(:name, "The school must have a name") unless name!=nil
 	end
